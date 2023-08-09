86a2c0ff4dd6c7d8819ba34b78b010444b2fee0d
Bdnature <84246065+Quddos@users.noreply.github.com>
2023-08-09 07:34:20 -0700

    routing parameters linking each post to post page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/',[\App\Http\Controllers\HomeController ::class, 'index'] )
 ->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
+
+Route::get('posts/{post}',
+    [\App\Http\Controllers\PostController::class,'show'])->name('posts.show');
